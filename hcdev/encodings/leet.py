3ff1f47e3c2494c7ac34cef7eeea3c86845e0d6e
noizeR3 <noizethehacker@hotmail.com>
2013-10-25 22:48:21 +0200

    Update leet.py

--- hcdev/encodings/leet.py
+++ hcdev/encodings/leet.py
@@ -1,15 +1,36 @@
 class leet:
-    def back(l,w):
-        return(replace(w,l))
-    def encode(self,text):
-        string = text.lower()
-        string = string.replace("a","4").replace("b","8").replace("c","<").replace("e","3").replace("g","9").replace("h","|-|")
-        string = string.replace("i","1").replace("k","|<").replace("l","|").replace("m","|\/|").replace("n","|\|").replace("o","0")
-        string = string.replace("q","0_").replace("s","5").replace("t","7").replace("u","|_|").replace("v","\/").replace("w","\/\/").replace("x","><").replace("z","2")
+    def encode(self,string):
+        alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','|','<','>','\\','/','-','G']
+        leet = ['4','8','<','>','3','f','9','|-|','1','j','k','|','|\/|','|\|','0','p','q','r','5','7','v','\/','w','x','y','2','o','i','z','e','a','s','G','t','b','g','l','c','d','m','n','-','6']
+
+        for i in range(0,len(alphabet)):
+            string = string.replace(alphabet[i],leet[i])
+
         return(string)
-    def decode(self,text):
-        string = text.lower()
-        string = string.replace("4","a").replace("8","b").replace("<","c").replace("3","e").replace("9","g").replace("|-|","h")
-        string = string.replace("1","i").replace("|<","k").replace("|\/|","m").replace("|\|","n").replace("|","l").replace("0","o")
-        string = string.replace("0_","q").replace("5","s").replace("7","t").replace("|_|","u").replace("\/\/","w").replace("\/","v").replace("><","x").replace("2","z")
+
+    def decode(self,string):
+        alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','|','<','>','\\','/','-','G']
+        leet = ['4','8','<','>','3','f','9','|-|','1','j','k','|','|\/|','|\|','0','p','q','r','5','7','v','\/','w','x','y','2','o','i','z','e','a','s','G','t','b','g','l','c','d','m','n','-','6']
+
+        for i in range(0,len(alphabet)):
+            string = string.replace(leet[i],alphabet[i])
+
         return(string)
+
+    def description(self):
+        return '''
+            The leet encoding algorithm is based upon the 1337 speak well-known in the hackers' underground.
+            Characters are replaced with numbers or groups of different characters resembling them.
+
+            Example input string:
+
+             Hello from LEET speak!
+
+            Encoded string:
+
+             |-|3||0 fr0|\/| |337 5p34|<!
+
+            To avoid messing up cases and to allow this algorithm to be used combined with other case-sensitive encodings, only lowercase characters, numbers and the |, <, >, _, -, \, /, G characters are translated (G is the only uppercase character that is translated).
+            
+            The "\" and "/" characters are respectively translated from plain text to leet as "m" and "n". This is just to avoid messups in the translation.
+        '''
