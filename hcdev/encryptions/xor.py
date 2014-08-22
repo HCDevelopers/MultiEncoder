0ff6cfe9b6fceb21bb79b90ce72eecc0f013d319
psychocoder <psychocoder@outlook.com>
2014-08-22 12:36:15 +0530

    refactored the code and bug fixed

--- hcdev/encryptions/xor.py
+++ hcdev/encryptions/xor.py
@@ -1,27 +1,34 @@
 #Author: Psycho_Coder
 
-import string,random
+from string import digits, letters
+from random import Random
+
 
 class HCPyEncoder:
 
-    def XORencrypt(self,plaintext,key):
-        cipher=""
+    def __init__(self):
+        pass
+
+    def XORencrypt(self, plaintext, key):
+
+        cipher = ""
         for chars in plaintext:
             for char in key:
-                chars=chr(ord(chars) ^ ord(char))
-            cipher=cipher+chars
+                chars = chr(ord(chars) ^ ord(char))
+            cipher = cipher+chars
         return cipher
 
-    def encode(self,plaintext,key):
-        cipher= self.XORencrypt(plaintext,key)      
+    def encode(self, plaintext, key):
+        cipher = self.XORencrypt(plaintext, key)
         return repr(cipher)
     
-    def decode(self,ciphertext,key):
-        plain= self.XORencrypt(ciphertext,key)
+    def decode(self, ciphertext, key):
+        plain = self.XORencrypt(ciphertext, key)
         return repr(plain)
 
-    def generateKey(self,keyLen=10):
-        pad = string.letters + string.digits
-        return ''.join(random.Random().sample(pad, keyLen))
+    @staticmethod
+    def generateKey(keyLen=10):
+        pad = letters + digits
+        return ''.join(Random().sample(pad, keyLen))
 
 xor = HCPyEncoder()
