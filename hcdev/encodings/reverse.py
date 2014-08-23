bfb6090fde8b86c1528b52bc118e6e3c4043cf5e
root <psychocoder@outlook.com>
2014-08-23 20:32:19 +0800

    removed annotation

--- hcdev/encodings/reverse.py
+++ hcdev/encodings/reverse.py
@@ -5,12 +5,10 @@ class Reverse:
     def __init__(self):
         pass
 
-    @staticmethod
-    def encode(text):
+    def encode(self, text):
         return text[::-1]
 
     def decode(self, text):
         return self.encode(text)
-
 
 reverse = Reverse()
