63d529bd57801b2191fe57596891f0621d8e429f
Psycho_Coder <PsychoCoderHC@users.noreply.github.com>
2014-08-23 14:57:20 +0530

    code reworked

--- hcdev/encodings/atbash.py
+++ hcdev/encodings/atbash.py
@@ -1,7 +1,7 @@
 class HCPyEncoder:
-    
-    def description(self):
-        print '''
+    @staticmethod
+    def description():
+        print('''
     Description :-
     ---------------
     The Atbash cipher is a very specific case of a Monoalphabetic substitution cipher where the letters of the alphabet are reversed. In otherwords, all As are replaced with Zs, all Bs are replaced with Ys, and so on. I have added support for encrypting digits and special characters too to make it stronger
@@ -11,18 +11,20 @@ class HCPyEncoder:
     Example:
     Plaintext  : Ex094, You are a very Nice Guy!
     Ciphertext: [liZeB?~uo?8r4?8?n4rk?_064?-okX
-    '''
-    
-    def __init__(self):
-        self.alphabets = ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+|:"<>-=[];,.?/`'                        
+    ''')
 
-    def encode(self,plaintext):
-        cipher=""        
+    def __init__(self):
+        self.alphabets = ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+|:"<>-=[];,.?/`'
+
+    def encode(self, plaintext):
+        cipher = ""
         for i in plaintext:
-            index=self.alphabets.index(i)
-            cipher=cipher+self.alphabets[abs(len(self.alphabets)-index-1)%len(self.alphabets)]    
+            index = self.alphabets.index(i)
+            cipher += self.alphabets[abs(len(self.alphabets) - index - 1) % len(self.alphabets)]
         return cipher
-    def decode(self,ciphertext):
+
+    def decode(self, ciphertext):
         return self.encode(ciphertext)
+
 
 atbash = HCPyEncoder()
